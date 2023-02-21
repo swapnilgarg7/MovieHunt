4d1f18403962834789c0cd4d5a3d625f77fb5c8a
swapnilgarg7 <swapnilgarg810@gmail.com>
2023-02-22 02:30:09 +0530

    error handling and loading added

---
+++ app/error.js
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({ error, reset }) {
+    useEffect(() => {
+        console.log("error: ", error);
+    }, [error]);
+
+
+    return (
+        <div className="error text-center mt-10 text-2xl space-y-4">
+            <h1>Oops! Something went wrong.</h1>
+            <button className="hover:text-yellow-500 bg-slate-300 dark:bg-gray-500 p-4" onClick={() => reset()}>
+                Try Again
+            </button>
+        </div>
+    );
+}
---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loading() {
+    return (
+        <div className='flex justify-center'>
+            <img className="h-96" src="loading.svg" alt="loading..." />
+        </div>
+    )
+}
+
+export default loading
